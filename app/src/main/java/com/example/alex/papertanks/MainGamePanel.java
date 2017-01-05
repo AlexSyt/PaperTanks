8765df2bb845e7d52c58f9c8727ab6b622a6acb1
Alex <asytnyk0@gmail.com>
2017-01-05 12:46:41 +0200

    add initialization of tanks

--- app/src/main/java/com/example/alex/papertanks/MainGamePanel.java
+++ app/src/main/java/com/example/alex/papertanks/MainGamePanel.java
@@ -1,21 +1,54 @@
 package com.example.alex.papertanks;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
+
 public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
+    private int displayWidth;
+    private int displayHeight;
+    private ArrayList<Tank> blueTanks;
+    private ArrayList<Tank> redTanks;
 
-    public MainGamePanel(Context context) {
+    public MainGamePanel(Context context, int displayWidth, int displayHeight) {
         super(context);
         getHolder().addCallback(this);
-        thread = new MainThread(getHolder(), this);
         setFocusable(true);
+        this.displayWidth = displayWidth;
+        this.displayHeight = displayHeight;
+        initTanks();
+    }
+
+    private void initTanks() {
+        blueTanks = new ArrayList<>();
+        redTanks = new ArrayList<>();
+        int tankWidth = 300;
+        int tankHeight = 300;
+
+        Bitmap blue = BitmapFactory.decodeResource(getResources(), R.drawable.blue_tank);
+        blue = Bitmap.createScaledBitmap(blue, tankWidth, tankHeight, false);
+        Bitmap red = BitmapFactory.decodeResource(getResources(), R.drawable.red_tank);
+        red = Bitmap.createScaledBitmap(red, tankWidth, tankHeight, false);
+
+        blueTanks.add(new Tank(blue, 0, 0, Team.BLUE));
+        blueTanks.add(new Tank(blue, 0, (displayHeight / 2) - (tankHeight / 2), Team.BLUE));
+        blueTanks.add(new Tank(blue, 0, displayHeight - tankHeight, Team.BLUE));
+
+        redTanks.add(new Tank(red, displayWidth - tankWidth, 0, Team.RED));
+        redTanks.add(new Tank(red, displayWidth - tankWidth, (displayHeight / 2) - (tankHeight / 2), Team.RED));
+        redTanks.add(new Tank(red, displayWidth - tankWidth, displayHeight - tankHeight, Team.RED));
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        thread = new MainThread(getHolder(), this);
         thread.setRunning(true);
         thread.start();
     }
@@ -36,6 +69,15 @@ public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback
             } catch (InterruptedException e) {
 
             }
+        }
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.WHITE);
+        for (int i = 0; i < 3; i++) {
+            blueTanks.get(i).draw(canvas);
+            redTanks.get(i).draw(canvas);
         }
     }
 }
--- app/src/main/java/com/example/alex/papertanks/Tank.java
+++ app/src/main/java/com/example/alex/papertanks/Tank.java
@@ -4,8 +4,6 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
 public class Tank {
-    private static final int BITMAP_WIDTH = 50;
-    private static final int BITMAP_HEIGHT = 50;
     private Bitmap bitmap;
     private int x;
     private int y;
@@ -16,7 +14,7 @@ public class Tank {
     private Team team;
 
     public Tank(Bitmap bitmap, int x, int y, Team team) {
-        this.bitmap = Bitmap.createScaledBitmap(bitmap, BITMAP_WIDTH, BITMAP_HEIGHT, false);
+        this.bitmap = bitmap;
         this.x = x;
         this.y = y;
         this.team = team;
@@ -84,14 +82,6 @@ public class Tank {
 
     public Team getTeam() {
         return team;
-    }
-
-    public static int getBitmapWidth() {
-        return BITMAP_WIDTH;
-    }
-
-    public static int getBitmapHeight() {
-        return BITMAP_HEIGHT;
     }
 
     public void draw(Canvas canvas) {
