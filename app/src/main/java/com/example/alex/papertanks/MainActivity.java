6b39627c937215ce9e8e2a82a0561ce1ab39c8d7
Alex <asytnyk0@gmail.com>
2017-01-05 12:43:09 +0200

    get screen width and height

--- app/src/main/java/com/example/alex/papertanks/MainActivity.java
+++ app/src/main/java/com/example/alex/papertanks/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.alex.papertanks;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(new MainGamePanel(this));
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        setContentView(new MainGamePanel(this, size.x, size.y));
     }
 }
